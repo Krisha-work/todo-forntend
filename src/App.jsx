38e7ac9a0b3e03af749f896aeb05267213938092
Krisha <workcom2050@gmail.com>
2025-03-26 14:47:54 +0530

    change is commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import "./App.css";
+import { useEffect } from "react";
 // import Home from "./components/main/Home";
 import MainBody from "./components/main/routes/MainBody";
 import NavbarSection from "./components/main/navbar/NavbarSection";
 
 function App() {
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
+
   return (
     <>
       {/* <div className="conatiner"> */}
-          <NavbarSection />
-          <MainBody />
+      <NavbarSection />
+      <MainBody />
       {/* </div> */}
     </>
   );
